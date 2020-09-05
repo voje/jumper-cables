46883691ca21ccc25fc4a9d56d6f3c9eaa00bac1
voje <>
2020-09-05 13:06:57 +0200

    Check named container with DockerCli, report health

--- go.mod
+++ go.mod
@@ -3,5 +3,15 @@ module voje/jumper-cables
 go 1.15
 
 require (
-	github.com/docker/docker master
+	github.com/containerd/containerd v1.4.0 // indirect
+	github.com/docker/distribution v2.7.1+incompatible // indirect
+	github.com/docker/docker v17.12.0-ce-rc1.0.20200901185902-ae0ef82b9035+incompatible
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.4.0 // indirect
+	github.com/gogo/protobuf v1.3.1 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/sirupsen/logrus v1.6.0
+	google.golang.org/grpc v1.31.1 // indirect
 )
--- jumpercables.go
+++ jumpercables.go
@@ -1,5 +1,59 @@
 package main
 
-func main() {
-	// TODO
+import (
+	"context"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/docker/docker/client"
+)
+
+// Global cli and context
+var cli *client.Client
+var ctx context.Context
+
+func init() {
+	// Init docker client and context.
+	var err error
+	cli, err = client.NewClientWithOpts(client.WithAPIVersionNegotiation())
+	if err != nil {
+		panic(err)
+	}
+	ctx = context.Background()
+
 }
+
+func containerIsUnhealthy(cntID string) bool {
+	info, err := cli.ContainerInspect(ctx, cntID)
+	if err != nil {
+		panic(err)
+	}
+
+	if info.ContainerJSONBase.State.Health.Status == "unhealthy" {
+		return true
+	}
+	return false
+}
+
+func watchContainer(cntID string, interval time.Duration) {
+	for {
+		if containerIsUnhealthy(cntID) {
+			log.Infof("Container %s is unhealthy - handling it!", cntID)
+		}
+
+		time.Sleep(interval)
+	}
+}
+
+func main() {
+	// Check once
+	containerName := "jumper-cables_unhealthy-server_1"
+	watchContainer(containerName, time.Second*3)
+}
+
+/*
+TODOS:
+- handle the unhealthy container
+- add system time to log messages
+*/
